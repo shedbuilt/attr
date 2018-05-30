677016a9c9466fb019d4d66e8ae7febd96a80db7
Auston Stewart <auston@shedbuilt.net>
2018-05-29 19:45:00 -1000

    Updating for blank

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sed -i -e 's|/@pkg_name@|&-@pkg_version@|' include/builddefs.in
-sed -i -e "/SUBDIRS/s|man[25]||g" man/Makefile
-sed -i 's:{(:\\{(:' test/run
+sed -i -e 's|/@pkg_name@|&-@pkg_version@|' include/builddefs.in &&
+sed -i -e "/SUBDIRS/s|man[25]||g" man/Makefile &&
+sed -i 's:{(:\\{(:' test/run &&
 ./configure --prefix=/usr \
-            --disable-static
-make -j $SHED_NUM_JOBS
-make DESTDIR=$SHED_FAKE_ROOT install install-dev install-lib
-chmod -v 755 ${SHED_FAKE_ROOT}/usr/lib/libattr.so
-mkdir -v ${SHED_FAKE_ROOT}/lib
-mv -v ${SHED_FAKE_ROOT}/usr/lib/libattr.so.* ${SHED_FAKE_ROOT}/lib
-ln -sfv ../../lib/$(readlink ${SHED_FAKE_ROOT}/usr/lib/libattr.so) ${SHED_FAKE_ROOT}/usr/lib/libattr.so
+            --disable-static &&
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install install-dev install-lib &&
+chmod -v 755 "${SHED_FAKE_ROOT}/usr/lib/libattr.so" &&
+mkdir -v "${SHED_FAKE_ROOT}/lib" &&
+mv -v "${SHED_FAKE_ROOT}"/usr/lib/libattr.so.* "${SHED_FAKE_ROOT}/lib" &&
+ln -sfv ../../lib/$(readlink "${SHED_FAKE_ROOT}/usr/lib/libattr.so") "${SHED_FAKE_ROOT}/usr/lib/libattr.so"
